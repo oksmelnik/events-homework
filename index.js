a030b8175a2f0facce9d94ba75d7922c09cf02cc
oksmelnik <melnik.oksana@gmail.com>
2018-03-04 13:57:23 +0100

    Add, get events

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+app.listen(4003, () => console.log('Express API listening on port 4003'))
+app.use(bodyParser.json())
+
+
+const Event = sequelize.define('event', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  sdate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+
+  edate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+}, {
+  tableName: 'events',
+  timestamps: false
+})
+const { Op } = require('sequelize')
+
+app.get('/events', (request, response) => {
+  Event.findAll(
+    {
+      where: {
+        sdate: {[Op.gte]: new Date()}
+      },
+      attributes: ['title', 'sdate', 'edate']
+    }
+  )
+  .then(events => {
+    response.send({ events })
+  })
+})
+
+
+app.get('/events/:id', (request, response) => {
+const eventId = request.params.id
+  Event.findById(eventId).then(events => {
+    response.send({ events })
+  })
+})
+
+
+app.post('/events', (req, res) => {
+  const event = req.body
+  console.log(event)
+   if (event.sdate > new Date() && event.sdate > event.edate ){
+  Event.create(event)
+    .then(entity => {
+      res.json(entity)
+      res.end()
+  })
+  .catch(err => {
+    res.status(422)
+    console.error(err);
+    res.json({ message: 'Oops! There was an error getting the . Please try again' })
+})
+}
+else {
+res.status(401).send({
+  message: 'Start date is not valid'
+})
+}
+
+})
